ac1b65c88d5a4535ed7d19f4890e05143dd76ff7
sarwar-asik <shasik2021@gmail.com>
2023-04-06 21:08:19 +0600

    mongodb projection in find

--- router/router1.js
+++ router/router1.js
@@ -14,8 +14,9 @@ app.get("getJob", async (req, res) => {
   try {
     // const getData = await Model1.find({ _id: "34n123412l12341234" }); ///or///
     // const getData = await Model1.find({$or:[{_id:"afa12312SD23E234",name:"dinku"}]})
+    // const getData = await Model1.find({status:{$ne:"married"}})
     const getData = await Model1.find({status:{$ne:"married"}})
-    
+
     res.status(200).send(getData)
     
   } catch (error) {
