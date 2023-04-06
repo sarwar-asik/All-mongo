0ef746495cf3009f0f8daa0acc6a501dc2c60ab2
sarwar-asik <shasik2021@gmail.com>
2023-04-06 21:54:47 +0600

    mongoose where and projection

--- router/router1.js
+++ router/router1.js
@@ -14,10 +14,25 @@ app.get("getJob", async (req, res) => {
   try {
     // const getData = await Model1.find({ _id: "34n123412l12341234" }); ///or///
     // const getData = await Model1.find({$or:[{_id:"afa12312SD23E234",name:"dinku"}]})
-    // const getData = await Model1.find({status:{$ne:"married"}})
-    const getData = await Model1.find({status:{$ne:"married"}})
+    // const getData = await Model1.find({status:{$ne:"married"}});
 
-    res.status(200).send(getData)
+
+
+
+
+//#### PProjections
+    // const getData = await Model1.find({},'name details email ')   /// it will show only name ,detail,email property ///
+    // const getData = await Model1.find({},'-name details -email ')   /// it will show without name and  email property ///
+    // const getData = await Model1.find({}).select({name:1})  /// it will show name property ///
+    const getData = await Model1.find({}).where("name").equal("Dinku")
+    .where("password").gt(123123)
+    .limit(3).sort({time:-1})
+
+    
+
+
+
+    res.status(200).send({success:true,data:getData})
     
   } catch (error) {
     res.status(400).send({ status: "failed", error: error.message });
