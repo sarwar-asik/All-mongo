87eee0949b9b706b596a7588679312f713d17ec0
sarwar-asik <shasik2021@gmail.com>
2023-04-06 21:05:52 +0600

    mongodb operatory

--- router/router1.js
+++ router/router1.js
@@ -1,18 +1,24 @@
-const app = require("express")
+const app = require("express");
 
-app.post("/addData",async(req,res)=>{
+app.post("/addData", async (req, res) => {
+  const newData = new Model1(req.body);
+  try {
+    //  const result =   newData.save()   /////or
+    const result = await Model1.create(req.body);
+  } catch (error) {
+    res.send({ status: 400, error: error.message });
+  }
+});
 
-    const newData =new Model1(req.body)
-    try{
-    //  const result =   newData.save()   /////or 
-    const result  =await Model1.create(req.body)
+app.get("getJob", async (req, res) => {
+  try {
+    // const getData = await Model1.find({ _id: "34n123412l12341234" }); ///or///
+    // const getData = await Model1.find({$or:[{_id:"afa12312SD23E234",name:"dinku"}]})
+    const getData = await Model1.find({status:{$ne:"married"}})
     
-
-
-
-
-    }
-    catch(error){
-    res.send({status:400,error:error.message})
-    }
-})
+    res.status(200).send(getData)
+    
+  } catch (error) {
+    res.status(400).send({ status: "failed", error: error.message });
+  }
+});
