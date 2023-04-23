ae2f46515316b21a6626710189f376d75544891b
sarwar-asik <shasik2021@gmail.com>
2023-04-23 18:52:16 +0600

    added schemas

--- MODEL/Brand.js
+++ MODEL/Brand.js
@@ -1,1 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const validator = require("validator")
+
+const brandSchema = mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:[true,'Provide a brand name'],
+        maxLength:100,
+        unique:true,
+        lowercase:true
+    },
+    description:String,
+    email:{
+        type:String,
+        validator:[validator.isEmail,"Please provide a valid Email"]
+    }
+})
--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require("dotenv").config()
-const app = express()
-const PORT = process.env.PORT || 8000
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const colors = require("colors");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(cors())
-app.use(express.json())
-mongoose.set("strictQuery",false)
+app.use(cors());
+app.use(express.json());
+mongoose.set("strictQuery", false);
 
 const url = process.env.URL;
 
+mongoose
+  .connect(url, {
+    dbName: "NodeMongo2Product",
+  })
+  .then(() => {
+    console.log("Connected DataBase".green.bold);
+  })
+  .catch((err) => {
+    const { name, stack, message } = err;
+    console.log("Not Connected Database ", { name, message, stack });
+  });
 
-app.get("/",async(req,res)=>{
-    res.send(`Product Server is Running on ${PORT}`)
-})
+app.get("/", async (req, res) => {
+  res.send(`Product Server is Running on ${PORT}`);
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is connected with ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is connected with ${PORT}`.green.bold);
+});
