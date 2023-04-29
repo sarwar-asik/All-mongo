7519d79f4019b0663a60c3a16b34f82ab55d62f0
sarwar-asik <shasik2021@gmail.com>
2023-04-29 12:52:58 +0600

    done StockModel

--- MODEL/Stock.js
+++ MODEL/Stock.js
@@ -2,12 +2,17 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const { ObjectId } = mongoose.Schema.Types;
 
-const ProductSchema = mongoose.Schema(
+const StockSchema = mongoose.Schema(
   {
+    productId: {
+      type: ObjectId,
+      ref: "Product",
+      required: true,
+    },
     name: {
       type: String,
       trim: true,
-      required: [true, "Provide a Product name"],
+      required: [true, "Provide a Stock name"],
       maxLength: [100, "Name is too longer"],
       unique: [true, "Name must be Unique"],
       minLength: [2, "Name must be 2 character."],
@@ -21,6 +26,12 @@ const ProductSchema = mongoose.Schema(
       required: true,
       min: [0, "Price cannot be negative"],
     },
+    quantity: {
+      type: String,
+      required: true,
+      min: [0, "Product quantity cannot be negative"],
+    },
+    
     unit: {
       type: String,
       required: true,
@@ -35,7 +46,7 @@ const ProductSchema = mongoose.Schema(
       validate: {
         validator: (value) => {
           if (!Array.isArray(value)) {
-            return false; 
+            return false;
           }
           let isValid = true;
           value.forEach((url) => {
@@ -52,6 +63,7 @@ const ProductSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+
     brand: {
       name: {
         type: String,
@@ -63,12 +75,52 @@ const ProductSchema = mongoose.Schema(
         required: true,
       },
     },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "status  can not be  {VALUES} ",
+      },
+    },
+    store:{
+        name: {
+            type: String,
+            trim: true,
+            required: [true, "Provide a Store name"],
+            maxLength: 100,
+            unique: true,
+            lowercase: true,
+            enum: {
+              values: ["dhaka", "chattogram","rajshahi","khulna", "barishal", "mymansing","rangpur", ],
+              message: "{VALUE} is not value name",
+            },
+          },
+          id:{
+            type:ObjectId,
+            required:true,
+            ref:"Store"
+          }
+    },
+    supplierBy:{
+        name:{
+            type: String,
+            trim: true,
+            required: [true, "Provide a Store name"]
+        },
+        id:{
+            type:ObjectId,
+            required:true,
+            ref:"Supplier"
+            
+        }
+    }
   },
   {
     timestamps: true,
   }
 );
 
-const ProductModel = mongoose.model("Product", ProductSchema);
+const StockModel = mongoose.model("Stock", StockSchema);
 
-export default ProductModel;
+export default StockModel;
